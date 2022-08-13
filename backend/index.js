48ff609434b597373b054a9b224d797651280dec
Sunju <57226409+sunjus@users.noreply.github.com>
2022-08-13 23:44:57 +0900

    /api/station/list

--- backend/index.js
+++ backend/index.js
@@ -15,18 +15,33 @@ const columnJourney = [
   "distance",
   "duration"
 ]
+const columnStation = [
+  "fid",
+  "id",
+  "nimi",
+  "namn",
+  "name",
+  "osoite",
+  "adress",
+  "kaupunki",
+  "stad",
+  "operaattor",
+  "kapasiteet",
+  "x",
+  "y"
+]
 
 app.get('/api/journey/list', (req, res) => {
   let page = Number(req.query.page);
   if (isNaN(page) || !Number.isInteger(page) || page <= 0) {
-    page =1;
+    page = 1;
   }
   let limit = Number(req.query.limit);
   if (isNaN(limit) || !Number.isInteger(limit) || limit <= 0) {
     limit = 100;
   }
 
-  let orderBy = req.query.orderby;
+  let orderBy = req.query.order_by;
   if (!columnJourney.includes(orderBy)) {
     orderBy = 'departure';
   }
@@ -39,6 +54,32 @@ app.get('/api/journey/list', (req, res) => {
         limit: limit,
         order_by: orderBy,
         data: journeyList
+    }
+  });
+});
+
+app.get('/api/station/list', (req, res) => {
+  let page = Number(req.query.page);
+  if (isNaN(page) || !Number.isInteger(page) || page <= 0) {
+    page = 1;
+  }
+  let limit = Number(req.query.limit);
+  if (isNaN(limit) || !Number.isInteger(limit) || limit <= 0) {
+    limit = 100;
+  }
+  let orderBy = req.query.order_by;
+  if (!columnStation.includes(orderBy)) {
+    orderBy = 'id';
+  }
+  const prepare = db.prepare(`SELECT * FROM station ORDER BY ${orderBy} ASC LIMIT ?, ?`);
+  const stationList = prepare.all((page - 1) * limit, limit);
+  res.json({
+    ok: true,
+    station: {
+      page: page,
+      limit: limit,
+      order_by: orderBy,
+      data: stationList
     }
   });
 });
