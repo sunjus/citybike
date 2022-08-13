c9323b8ff81fe9f7f65743d194bac107ab36be75
Sunju <57226409+sunjus@users.noreply.github.com>
2022-08-14 01:41:30 +0900

    Add sample link to stationList table

--- frontend/src/components/StationList.js
+++ frontend/src/components/StationList.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { DataGrid } from '@mui/x-data-grid';
 
 const stationColumns = [
   { field: 'id', headerName: 'ID' },
   { field: 'fid', headerName: 'FID' },
-  { field: 'nimi', headerName: 'Nimi' },  
+  { field: 'nimi', headerName: 'Nimi', 
+    renderCell: (props) => (<Link to={`/station/${props.row.id}`}>{props.value}</Link>)  
+  },  
   { field: 'osoite', headerName: 'Osoite', width: 200 },
   { field: 'kaupunki', headerName: 'Kaupunki', width: 100 },
   { field: 'stad', headerName: 'Stad', width: 100 },
