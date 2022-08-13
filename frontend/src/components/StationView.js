f3297cce5816f29225fc3e9296798eb0c038369e
Sunju <57226409+sunjus@users.noreply.github.com>
2022-08-14 02:08:45 +0900

    Add station view

--- frontend/src/components/StationView.js
+++ frontend/src/components/StationView.js
@@ -3,23 +3,11 @@ import { useParams } from "react-router-dom";
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
-//  { field: 'departure', headerName: 'Departure', valueGetter: (v) => v ? (new Date(v)).toISOString(): 'N/A' },
-function getDateString(obj) {
-  const v = obj.value;
-  return v ? (new Date(v)).toISOString() : 'N/A';
-}
-
-const stationViewColumns = [
-  { field: 'id', headerName: 'ID' },
-  { field: 'departure', headerName: 'Departure', width: 200, valueGetter: getDateString },  
-  { field: 'return', headerName: 'Return', width: 200, valueGetter: getDateString },
-  { field: 'departure_station_name', headerName: 'Depareure Station', width: 150 },
-  { field: 'return_station_name', headerName: 'Return Station', width: 150 }
-];
-
 const StationView = () => {
   const { id } = useParams();
   const [station, setStation] = useState(null);
+  const [nDepartures, setNDepartures] = useState(0);
+  const [nReturns, setNReturns] = useState(0);
   const urlStationView = `http://localhost:3000/api/station/view?id=${id}`;
   
   useEffect(() => {
@@ -28,20 +16,23 @@ const StationView = () => {
     .then((data) => {
     console.log(data) 
     setStation(data)
-    }
-    )   
+    setNDepartures(data)
+    setNReturns(data)
+    })   
     .catch((err) => console.log(err))
   },[id]);
   
   useEffect(() => {
-    console.log(station?.station?.name)
+    console.log(station?.station?.name, station?.number_of_departure)
   }, [station]);
 
   return (
     <div>
       <Container maxWidth="sm" height="80vh">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '10vh' }}>Station Name:{station ? station?.station?.name : <div>Loading...</div>}</Box>
-        <Box sx={{ bgcolor: '#ffc0cb', height: '10vh' }}>Address: {station ? station?.station?.osoite : <div>Loading...</div>}</Box>
+        <Box sx={{ bgcolor: '#ffa4b3', height: '10vh' }}>Station Name:{station ? station?.station?.name : <div>Loading...</div>}</Box>
+        <Box sx={{ bgcolor: '#ffb1be', height: '10vh' }}>Address: {station ? station?.station?.osoite : <div>Loading...</div>}</Box>
+        <Box sx={{ bgcolor: '#ffc0cb', height: '10vh' }}>Total number of journeys starting from the station: {station ? station?.number_of_departure : <div>Loading...</div>}</Box>
+        <Box sx={{ bgcolor: '#ffd2d7', height: '10vh' }}>Total number of journeys ending at the station: {station ? station?.number_of_return : <div>Loading...</div>}</Box>
       </Container>
     </div>
   );
