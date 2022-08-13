371c92f2106ebdf495904e66a6d2773a31447bbd
Sunju <57226409+sunjus@users.noreply.github.com>
2022-08-14 00:37:14 +0900

    Test basic mui data-grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,46 @@
-import { useEffect } from 'react';
+import { useState,useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 import './App.css';
 
+//  { field: 'departure', headerName: 'Departure', valueGetter: (v) => v ? (new Date(v)).toISOString(): 'N/A' },
+function getDateString(obj) {
+  const v = obj.value;
+  return v ? (new Date(v)).toISOString() : 'N/A';
+}
+
+const journeyColumns = [
+  { field: 'id', headerName: 'ID' },
+  { field: 'departure', headerName: 'Departure', valueGetter: getDateString },  
+  { field: 'return', headerName: 'Return', valueGetter: getDateString },
+  { field: 'departure_station_name', headerName: 'Depareure Station' },
+  { field: 'return_station_name', headerName: 'Return Station' },
+  { field: 'distance', headerName: 'Distance [m]' },
+  { field: 'duration', headerName: 'Duration [s]' }
+];
+
 function App() {
+  const [journeyList, setJourneyList] = useState(null);
   const url = "http://localhost:3000/api/journey/list";
   useEffect(() => {
     fetch(url)
-    .then((data) => console.log(data))
+    .then((response) => response.json())
+    .then((data) => setJourneyList(data))
     .catch((err) => console.log(err))
   },[])
+
+  useEffect(() => {
+    console.log(journeyList?.journey?.data);
+  }, [journeyList])
   return (
     <div className="App">
       <header className="App-header">
         CITYBIKE
       </header>
+      <main>
+        <div style={{ height: 800, width: '100%' }}>
+          <DataGrid rows={journeyList?.journey?.data || []} columns={journeyColumns} />
+        </div>
+      </main>
     </div>
   );
 }
