dbfb20efae29fedc90ef68baa6c4e7feac167b35
Sunju <57226409+sunjus@users.noreply.github.com>
2022-08-14 01:07:57 +0900

    Implement stationList

--- frontend/src/components/JourneyList.js
+++ frontend/src/components/JourneyList.js
@@ -3,25 +3,25 @@ import { DataGrid } from '@mui/x-data-grid';
 
 //  { field: 'departure', headerName: 'Departure', valueGetter: (v) => v ? (new Date(v)).toISOString(): 'N/A' },
 function getDateString(obj) {
-    const v = obj.value;
-    return v ? (new Date(v)).toISOString() : 'N/A';
-  }
+  const v = obj.value;
+  return v ? (new Date(v)).toISOString() : 'N/A';
+}
 
-  function getDistanceString(obj) {
-    const v = obj.value;
-    if (!v) return 'N/A';
-    return `${(v/1000).toFixed(2)} km`
-  }
+function getDistanceString(obj) {
+  const v = obj.value;
+  if (!v) return 'N/A';
+  return `${(v/1000).toFixed(2)} km`
+}
 
-  const journeyColumns = [
-    { field: 'id', headerName: 'ID' },
-    { field: 'departure', headerName: 'Departure', width: 200, valueGetter: getDateString },  
-    { field: 'return', headerName: 'Return', width: 200, valueGetter: getDateString },
-    { field: 'departure_station_name', headerName: 'Depareure Station', width: 150 },
-    { field: 'return_station_name', headerName: 'Return Station', width: 150 },
-    { field: 'distance', headerName: 'Distance', valueGetter: getDistanceString },
-    { field: 'duration', headerName: 'Duration [s]', valueGetter: (v) => `${v.value} s` }
-  ];
+const journeyColumns = [
+  { field: 'id', headerName: 'ID' },
+  { field: 'departure', headerName: 'Departure', width: 200, valueGetter: getDateString },  
+  { field: 'return', headerName: 'Return', width: 200, valueGetter: getDateString },
+  { field: 'departure_station_name', headerName: 'Depareure Station', width: 150 },
+  { field: 'return_station_name', headerName: 'Return Station', width: 150 },
+  { field: 'distance', headerName: 'Distance', valueGetter: getDistanceString },
+  { field: 'duration', headerName: 'Duration [s]', valueGetter: (v) => `${v.value} s` }
+];
 
 function JourneyList() {
   const [journeyList, setJourneyList] = useState(null);
@@ -32,17 +32,17 @@ function JourneyList() {
     .then((response) => response.json())
     .then((data) => setJourneyList(data))
     .catch((err) => console.log(err))
-  },[])
+  },[]);
 
   useEffect(() => {
     console.log(journeyList?.journey?.data)
-  }, [journeyList])
+  }, [journeyList]);
 
   return (
     <div style = {{width: '100%', height: '100vh'}}>
       <DataGrid rows={journeyList?.journey?.data || []} columns={journeyColumns} />
     </div>
-  )
+  );
 }
 
 export default JourneyList;
--- frontend/src/components/StationList.js
+++ frontend/src/components/StationList.js
@@ -1,9 +1,40 @@
+import { useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+//  { field: 'departure', headerName: 'Departure', valueGetter: (v) => v ? (new Date(v)).toISOString(): 'N/A' },  
+const stationColumns = [
+  { field: 'id', headerName: 'ID' },
+  { field: 'fid', headerName: 'FID' },
+  { field: 'nimi', headerName: 'Nimi' },  
+  { field: 'osoite', headerName: 'Osoite', width: 200 },
+  { field: 'kaupunki', headerName: 'Kaupunki', width: 100 },
+  { field: 'stad', headerName: 'Stad', width: 100 },
+  { field: 'operaattor', headerName: 'Operaattor', width: 150 },
+  { field: 'kapasiteet', headerName: 'Kapasiteet' },
+  { field: 'x', headerName: 'X', width: 100 },
+  { field: 'y', headerName: 'Y', width: 100 }
+];
+
 function StationList() {
+  const [stationList, setStationList] = useState([]);
+  const urlStation = "http://localhost:3000/api/station/list";
+
+  useEffect(() => {
+    fetch(urlStation)
+    .then((response) => response.json())
+    .then((data) => setStationList(data))
+    .catch((err) => console.log(err))
+  },[]);
+
+  useEffect(() => {
+    console.log(stationList?.station?.data)
+  }, [stationList]);
+
   return (
-    <div>
-      stationList
+    <div style = {{width: '100%', height: '100vh'}}>
+      <DataGrid rows={stationList?.station?.data || []} columns={stationColumns} />
     </div>
-  )
+  );
 }
 
 export default StationList;
