78c339d66c39d0c8ddfc559d9c2ec61d054c6778
Sunju <57226409+sunjus@users.noreply.github.com>
2022-08-13 18:26:08 +0900

    Import journey to DB

--- utils/import_to_sqlite3.js
+++ utils/import_to_sqlite3.js
@@ -1,28 +1,41 @@
 const Database = require('better-sqlite3');
-const { readCSVStations } = require('./read_csv');
-const db = new Database('../database/data.db');
+const { readCSVStations, readCSVJourney } = require('./read_csv');
+const dbPath = '../database/data.db';
+const db = new Database(dbPath);
 
 function createTables() {
+  console.log(`== Import Tables: ${dbPath}`);
   db.exec(`CREATE TABLE IF NOT EXISTS station (
-      fid INTEGER NOT NULL PRIMARY KEY,
-      id INTEGER NOT NULL UNIQUE,
-      nimi TEXT,
-      namn TEXT,
-      name TEXT,
-      osoite TEXT,
-      adress TEXT,
-      kaupunki TEXT,
-      stad TEXT,
-      operaattor TEXT,
-      kapasiteet INTEGER,
-      x TEXT,
-      y TEXT
-    )
-  `);
+    fid INTEGER NOT NULL PRIMARY KEY,
+    id INTEGER NOT NULL UNIQUE,
+    nimi TEXT,
+    namn TEXT,
+    name TEXT,
+    osoite TEXT,
+    adress TEXT,
+    kaupunki TEXT,
+    stad TEXT,
+    operaattor TEXT,
+    kapasiteet INTEGER,
+    x TEXT,
+    y TEXT
+  )`);
+  db.exec(`CREATE TABLE IF NOT EXISTS journey (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    departure INTEGER,
+    return INTEGER,
+    departure_station_id INTEGER,
+    departure_station_name TEXT,
+    return_station_id INTEGER,
+    return_station_name TEXT,
+    distance INTEGER,
+    duration INTEGER
+  )`);
 }
 
-async function importStations() {
-  const records = readCSVStations('../data/726277c507ef4914b0aec3cbcfcbfafc_0.csv');
+async function importStations(csvPath) {
+  console.log(`== Import Stations: ${csvPath}`);
+  const records = readCSVStations(csvPath);
   const prepare = db.prepare('INSERT INTO station VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
   let count = 0;
   for await (const record of records) {
@@ -33,5 +46,44 @@ async function importStations() {
   console.log("count = ", count, countDB);
 }
 
-createTables();
-importStations();
+async function importJourney(csvPath) {
+  console.log(`== Import Journey: ${csvPath}`);
+  const records = readCSVJourney(csvPath);
+  const prepare = db.prepare(`INSERT INTO journey
+    (departure,
+    return,
+    departure_station_id,
+    departure_station_name,
+    return_station_id,
+    return_station_name,
+    distance,
+    duration)
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?)
+  `)
+  const transaction = db.transaction(records => records.forEach((record) => prepare.run(record)))
+  let recordsSub = [];
+  let count = 0;
+  for await (const record of records) {
+    recordsSub.push(record);
+    count++;
+    if (count % 100000 === 0) {
+      console.log(`processing ... ${count} ...`);
+      if (recordsSub.length > 0) transaction(recordsSub);
+      recordsSub = [];
+    } 
+  }
+  if (recordsSub.length > 0) transaction(recordsSub);
+
+  const countDB = db.prepare('SELECT count(*) as counst FROM journey').get();
+  console.log("count = ", count, countDB);
+}
+  
+async function importToDatabase() {
+  createTables();
+  await importStations('../data/726277c507ef4914b0aec3cbcfcbfafc_0.csv');
+  await importJourney('../data/2021-05.csv');
+  await importJourney('../data/2021-06.csv');
+  await importJourney('../data/2021-06.csv');
+}
+  
+importToDatabase();
--- utils/read_csv.js
+++ utils/read_csv.js
@@ -5,10 +5,33 @@ function readCSVStations(csvPath) {
   const parser = fs
     .createReadStream(csvPath)
     .pipe(csv.parse({ from: 2 }))
+    .pipe(csv.transform((record) => {
+      return record.map((field) => field.trim())
+    }))
+
+  return parser;
+}
+
+function readCSVJourney(csvPath) {
+  const parser = fs
+    .createReadStream(csvPath)
+    .pipe(csv.parse({ from: 2 }))
+    .pipe(csv.transform((record) => {
+      // record[6] : distance
+      // record[7] : duration
+      if (record[6] < 10 || record[7] < 10) return null;
+      // trim each field.
+      const record_trim = record.map((field) => field.trim());
+      // convert datetime string to epoch time(milisecond/integer)
+      record_trim[0] = new Date(record_trim[0] + '+0300').getTime();
+      record_trim[1] = new Date(record_trim[1] + '+0300').getTime();
+      return record_trim;
+    }))
 
   return parser;
 }
 
 module.exports = {
-  readCSVStations
+  readCSVStations,
+  readCSVJourney
 }
